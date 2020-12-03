46179c7722287752b7933eabd364225105bf5c9b
MZ <nikita.zholkovskyi@gmail.com>
2020-12-03 13:24:13 +0100

    displaying posters on page

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ var MovieService = /** @class */ (function () {
 }());
 var apiClient = new MovieService();
 apiClient.getMovies("rock").then(function (data) { return (console.log(data)); });
+function displayMovies() {
+    apiClient.getMovies("lol").then(function (data) {
+        var movies = data;
+        for (var i = 0; i < movies.length; i++) {
+            if (movies[i].poster === "N/A") {
+                movies[i].poster = "no-poster.jpg";
+            }
+            posters.innerHTML += '<div class="cards mt-4"> <img style="height: 300px; width: 200px" src=' + movies[i].poster + '> </src></div>';
+        }
+    });
+}
+displayMovies();
--- script.ts
+++ script.ts
@@ -25,4 +25,17 @@ class MovieService {
 
 const apiClient = new MovieService();
 
-apiClient.getMovies("rock").then((data) => (console.log(data)))
+apiClient.getMovies("rock").then((data) => (console.log(data)));
+
+function displayMovies() {
+    apiClient.getMovies("lol").then((data) => {
+        let movies = data;
+        for (let i = 0; i < movies.length; i++) {
+            if (movies[i].poster=== "N/A"){
+                movies[i].poster = "no-poster.jpg"
+            }
+            posters.innerHTML += '<div class="cards mt-4"> <img style="height: 300px; width: 200px" src=' + movies[i].poster + '> </src></div>'
+        }
+    })
+}
+displayMovies()
