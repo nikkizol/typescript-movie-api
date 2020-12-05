0d872f89493d9be19b29262a2b3909e27b68396b
MZ <nikita.zholkovskyi@gmail.com>
2020-12-05 16:54:54 +0100

    optimized script

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let apiKey = new Key();
 const posters = document.querySelector("#poster");
 const input = document.getElementById("inputTitle");
 const errorMsg = document.getElementById("errorMsg");
+const searchButton = document.getElementById('run');
+const imgNoPoster = "no-poster.jpg";
+const id = '';
+const title = '';
 class Movie {
     constructor(title, poster, id) {
         this.title = title;
@@ -10,64 +14,77 @@ class Movie {
         this.id = id;
     }
 }
-function formatMovie(movie) {
-    return { title: movie.Title, poster: movie.Poster, id: movie.imdbID };
-}
-function openMovieOnNewPage(allPosters, data) {
-    allPosters.forEach((poster) => poster.addEventListener('click', function () {
-        posters.innerHTML = "";
-        const title = '';
-        displayMovies(poster.childNodes[0].firstChild.nodeValue, title);
-    }));
-}
-class MovieServiceById {
+class MovieSearchById {
     getMovies(id) {
         return fetch(`http://www.omdbapi.com/?&apikey=${apiKey.getKey()}&i=${id}`)
             .then(res => res.json());
     }
 }
-class MovieServiceByTitle {
+class MovieSearchByTitle {
     getMovies(title) {
         return fetch(`http://www.omdbapi.com/?&apikey=${apiKey.getKey()}&s=${title}`)
             .then(res => res.json())
             .then(res => res.Search.map((movie) => formatMovie(movie)));
     }
 }
+function msgError() {
+    return '<div class="alert alert-light" role="alert">' + "Please enter correct movie title" + '</div>';
+}
+function displayingMoviesInHtml(movies, i) {
+    return '<div class="cards mt-4"><div class="movieId">' + movies[i].id + '</div><div class="overlay">' + movies[i].title + '</div><img style="height: 300px; width: 200px" src=' + movies[i].poster + '> </src></div>';
+}
+function displayingSelectedMovieInHtml(movies) {
+    return '<div class="cards mt-4"><div class="movieId">' + movies.Title + '</div><div class="overlay">' + movies.Title + '</div><img style="height: 300px; width: 200px" src=' + movies.Poster + '> </src></div>';
+}
+function resetDOM() {
+    errorMsg.innerHTML = "";
+    posters.innerHTML = "";
+}
 function selectMovieClass(id, title) {
     let apiClient;
     if (title == "") {
-        apiClient = new MovieServiceById().getMovies(id);
+        apiClient = new MovieSearchById().getMovies(id);
     }
     else {
-        apiClient = new MovieServiceByTitle().getMovies(title);
+        apiClient = new MovieSearchByTitle().getMovies(title);
     }
     return apiClient;
+}
+function openMovieOnNewPage(allPosters, data) {
+    allPosters.forEach((poster) => poster.addEventListener('click', function () {
+        resetDOM();
+        displayMovies(poster.childNodes[0].firstChild.nodeValue, title);
+    }));
+}
+function formatMovie(movie) {
+    return { title: movie.Title, poster: movie.Poster, id: movie.imdbID };
 }
 function displayMovies(id, title) {
     selectMovieClass(id, title).then((data) => {
         let movies = data;
         if (movies.length == undefined) {
-            return posters.innerHTML += '<div class="cards mt-4"><div class="movieId">' + movies.Title + '</div><div class="overlay">' + movies.Title + '</div><img style="height: 300px; width: 200px" src=' + movies.Poster + '> </src></div>';
+            return posters.innerHTML += displayingSelectedMovieInHtml(movies);
         }
         else
             for (let i = 0; i < movies.length; i++) {
                 if (movies[i].poster === "N/A") {
-                    movies[i].poster = "no-poster.jpg";
+                    movies[i].poster = imgNoPoster;
                 }
-                posters.innerHTML += '<div class="cards mt-4"><div class="movieId">' + movies[i].id + '</div><div class="overlay">' + movies[i].title + '</div><img style="height: 300px; width: 200px" src=' + movies[i].poster + '> </src></div>';
+                posters.innerHTML += displayingMoviesInHtml(movies, i);
             }
         const allPosters = document.querySelectorAll(".cards");
         openMovieOnNewPage(allPosters, movies);
-        //     posters.innerHTML += '<div class="cards mt-4"><div class="movieId">' + movies.Title + '</div><div class="overlay">' + movies.Title + '</div><img style="height: 300px; width: 200px" src=' + movies.Poster + '> </src></div>'
     }).catch(error => {
         console.error(error);
-        errorMsg.innerHTML = '<div class="alert alert-light" role="alert">' + "Please enter correct movie title" + '</div>';
+        errorMsg.innerHTML = msgError();
     });
 }
-document.getElementById('run').addEventListener('click', function (event) {
+searchButton.addEventListener('click', function (event) {
     event.preventDefault();
-    errorMsg.innerHTML = "";
-    posters.innerHTML = "";
-    const id = '';
+    resetDOM();
+    if (input.value == "") {
+        errorMsg.innerHTML = msgError();
+        posters.removeChild(posters);
+    }
     displayMovies(id, input.value);
 });
--- script.ts
+++ script.ts
@@ -5,6 +5,10 @@ let apiKey = new Key();
 const posters = document.querySelector("#poster")!;
 const input = (<HTMLInputElement>document.getElementById("inputTitle"));
 const errorMsg = document.getElementById("errorMsg")!;
+const searchButton = document.getElementById('run')!;
+const imgNoPoster = "no-poster.jpg";
+const id = '';
+const title = '';
 
 class Movie {
     title: string;
@@ -19,26 +23,14 @@ class Movie {
     }
 }
 
-function formatMovie(movie: any): Movie {
-    return {title: movie.Title, poster: movie.Poster, id: movie.imdbID}
-}
-
-function openMovieOnNewPage(allPosters: any, data: any) {
-    allPosters.forEach((poster: any) => poster.addEventListener('click', function () {
-        posters.innerHTML = "";
-        const title = '';
-        displayMovies(poster.childNodes[0].firstChild.nodeValue, title)
-    }));
-}
-
-class MovieServiceById {
+class MovieSearchById {
     getMovies(id: string): Promise<Movie[]> {
         return fetch(`http://www.omdbapi.com/?&apikey=${apiKey.getKey()}&i=${id}`)
             .then(res => res.json())
     }
 }
 
-class MovieServiceByTitle {
+class MovieSearchByTitle {
     getMovies(title: string): Promise<Movie[]> {
         return fetch(`http://www.omdbapi.com/?&apikey=${apiKey.getKey()}&s=${title}`)
             .then(res => res.json())
@@ -46,46 +38,71 @@ class MovieServiceByTitle {
     }
 }
 
+function msgError() {
+    return '<div class="alert alert-light" role="alert">' + "Please enter correct movie title" + '</div>'
+}
+
+function displayingMoviesInHtml(movies: any, i: number) {
+    return '<div class="cards mt-4"><div class="movieId">' + movies[i].id + '</div><div class="overlay">' + movies[i].title + '</div><img style="height: 300px; width: 200px" src=' + movies[i].poster + '> </src></div>'
+}
+
+function displayingSelectedMovieInHtml(movies: any) {
+    return '<div class="cards mt-4"><div class="movieId">' + movies.Title + '</div><div class="overlay">' + movies.Title + '</div><img style="height: 300px; width: 200px" src=' + movies.Poster + '> </src></div>'
+}
+
+function resetDOM() {
+    errorMsg.innerHTML = "";
+    posters.innerHTML = "";
+}
+
 function selectMovieClass(id: string, title: string) {
     let apiClient;
     if (title == "") {
-        apiClient = new MovieServiceById().getMovies(id)
+        apiClient = new MovieSearchById().getMovies(id)
     } else {
-        apiClient = new MovieServiceByTitle().getMovies(title)
+        apiClient = new MovieSearchByTitle().getMovies(title)
     }
     return apiClient;
 }
 
+function openMovieOnNewPage(allPosters: any, data: any) {
+    allPosters.forEach((poster: any) => poster.addEventListener('click', function () {
+        resetDOM()
+        displayMovies(poster.childNodes[0].firstChild.nodeValue, title)
+    }))
+}
 
-function displayMovies(id: string, title: string,) {
+function formatMovie(movie: any): Movie {
+    return {title: movie.Title, poster: movie.Poster, id: movie.imdbID}
+}
+
+function displayMovies(id: string, title: string) {
     selectMovieClass(id, title).then((data) => {
         let movies = data;
         if (movies.length == undefined) {
-            return posters.innerHTML += '<div class="cards mt-4"><div class="movieId">' + movies.Title + '</div><div class="overlay">' + movies.Title + '</div><img style="height: 300px; width: 200px" src=' + movies.Poster + '> </src></div>'
+            return posters.innerHTML += displayingSelectedMovieInHtml(movies)
         } else
             for (let i = 0; i < movies.length; i++) {
                 if (movies[i].poster === "N/A") {
-                    movies[i].poster = "no-poster.jpg"
+                    movies[i].poster = imgNoPoster;
                 }
-                posters.innerHTML += '<div class="cards mt-4"><div class="movieId">' + movies[i].id + '</div><div class="overlay">' + movies[i].title + '</div><img style="height: 300px; width: 200px" src=' + movies[i].poster + '> </src></div>'
+                posters.innerHTML += displayingMoviesInHtml(movies, i)
             }
         const allPosters = document.querySelectorAll(".cards")!;
         openMovieOnNewPage(allPosters, movies);
-
-
-        //     posters.innerHTML += '<div class="cards mt-4"><div class="movieId">' + movies.Title + '</div><div class="overlay">' + movies.Title + '</div><img style="height: 300px; width: 200px" src=' + movies.Poster + '> </src></div>'
     }).catch(error => {
         console.error(error);
-        errorMsg.innerHTML = '<div class="alert alert-light" role="alert">' + "Please enter correct movie title" + '</div>'
+        errorMsg.innerHTML = msgError()
     })
-
 }
 
-document.getElementById('run')!.addEventListener('click', function (event) {
+searchButton.addEventListener('click', function (event) {
     event.preventDefault();
-    errorMsg.innerHTML = "";
-    posters.innerHTML = "";
-    const id = '';
+    resetDOM()
+    if (input.value == "") {
+        errorMsg.innerHTML = msgError()
+        posters.removeChild(posters);
+    }
     displayMovies(id, input.value)
 });
 
